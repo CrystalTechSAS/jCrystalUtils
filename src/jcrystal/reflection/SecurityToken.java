754f1f86559fff3f84a3cc94fd38fb849b64a117
gasotelo <gasotelo@Germans-MacBook-Pro.local>
2017-06-14 19:07:28 +0200

    Inicio soporte JSF

--- src/jcrystal/reflection/SecurityToken.java
+++ src/jcrystal/reflection/SecurityToken.java
@@ -4,4 +4,6 @@ package jcrystal.reflection;
  * Created by g on 9/6/16.
  */
 public interface SecurityToken {
+	String token();
+	long rol();
 }
---
+++ src/jcrystal/reflection/annotations/validation/Validate.java
@@ -0,0 +1,11 @@
+package jcrystal.reflection.annotations.validation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Validate {
+	boolean notEmpty() default false;
+	int min() default Integer.MIN_VALUE;
+	int max() default Integer.MAX_VALUE;
+}
