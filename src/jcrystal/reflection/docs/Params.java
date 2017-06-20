1822fe68c306863e004d5bc6be3c0eaabc17aba4
jcrystaldev1 <ga.sotelo@gmail.com>
2017-06-19 19:00:34 -0500

    Cambios en las anotaciones de la documentación

--- src/jcrystal/reflection/docs/Params.java
+++ src/jcrystal/reflection/docs/Params.java
@@ -1,13 +1,9 @@
 package jcrystal.reflection.docs;
 
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Param {
-
-	String name();
-	String value();
-	String test();
+public @interface Params {
+	 Param[] value();
 }
