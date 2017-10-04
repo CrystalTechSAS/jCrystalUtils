32616d003e9f92fdd5b70f5d9e91bde149c3c874
gasotelo <gasotelo@gmail.com>
2017-10-04 16:23:23 -0500

    Queues

---
+++ src/jcrystal/datetime/DateType.java
@@ -0,0 +1,21 @@
+package jcrystal.datetime;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+public enum DateType {
+	DATE_TIME("yyyyMMddHHmm"),
+    TIME("HHmm"),
+    DATE("yyyyMMddHHmm"),
+    DATE_SECONDS("yyyyMMddHHmmss"),
+    TIME_SECONDS("HHmmss"),
+    DATE_MILIS("yyyyMMddHHmmssSSS"),
+    TIME_MILIS("HmmssSSS");
+
+    public final SimpleDateFormat FORMAT;
+    public final String format;
+    DateType(String format){
+        FORMAT = new SimpleDateFormat(this.format = format);
+        FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+}
--- src/jcrystal/reflection/annotations/CrystalDate.java
+++ src/jcrystal/reflection/annotations/CrystalDate.java
@@ -3,6 +3,9 @@ package jcrystal.reflection.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import jcrystal.datetime.DateType;
+
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CrystalDate {
+	DateType value() default DateType.DATE;
 }
