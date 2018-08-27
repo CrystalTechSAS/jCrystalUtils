002b5621e7fde91de93bffea999f7e11d49e403b
gasotelo <gasotelo@gmail.com>
2018-08-27 10:08:34 -0500

    Json para Rel 1 to 1

--- src/jcrystal/reflection/annotations/entities/Rel1to1.java
+++ src/jcrystal/reflection/annotations/entities/Rel1to1.java
@@ -3,8 +3,11 @@ package jcrystal.reflection.annotations.entities;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import jcrystal.json.JsonLevel;
+
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Rel1to1 {
 	String name() default "";
 	String target() default "";
+	JsonLevel keyLevel() default JsonLevel.NONE;
 }
