bf033a40d25a5fdb8608d487d481fba7aa963d28
gasotelo <gasotelo@gmail.com>
2019-01-23 12:38:10 -0500

    Creación sublist

---
+++ src/jcrystal/clients/admin/SubListOption.java
@@ -0,0 +1,14 @@
+package jcrystal.clients.admin;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value= {ElementType.METHOD})
+public @interface SubListOption {
+	String name() default "Opción extra";
+	String icon() default "question";
+	Class<?> sublistClass();
+}
