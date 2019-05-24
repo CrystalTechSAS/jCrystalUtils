35d3219563074465a35fc6d925e04092edbe170b
gasotelo <gasotelo@gmail.com>
2019-05-23 21:09:34 -0500

    HeaderParam creation

---
+++ src/jcrystal/reflection/annotations/ws/HeaderParam.java
@@ -0,0 +1,16 @@
+/* Copyright (C) Germán Augusto Sotelo Arévalo - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Germán Augusto Sotelo Arévalo <gasotelo@crystaltech.co>, December 2018
+ */
+package jcrystal.reflection.annotations.ws;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value= {ElementType.PARAMETER, ElementType.FIELD})
+public @interface HeaderParam {
+}
