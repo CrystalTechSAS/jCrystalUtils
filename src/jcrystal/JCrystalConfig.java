0ca7a312acd789d8cf79192194f987fb0e39ff0a
gasotelo <gasotelo@gmail.com>
2019-08-17 13:38:24 -0500

    Update JCrystalConfig.java

--- src/jcrystal/JCrystalConfig.java
+++ src/jcrystal/JCrystalConfig.java
@@ -7,5 +7,5 @@ package jcrystal;
 
 public abstract class JCrystalConfig {
 	public static ServerConfig SERVER = new ServerConfig();
-	public static String JCRYSTAL_SERVER_URL = "https://jcrystal-213302.appspot.com";
+	public static String JCRYSTAL_SERVER_URL = "https://1-dot-back-dot-jcrystal-213302.appspot.com";
 }
