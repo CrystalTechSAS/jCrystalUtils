4e5338c250e929d1fe385c5e3f911fbd2dff4cef
gasotelo <gasotelo@gmail.com>
2020-01-19 21:08:27 -0500

    Update DebugInfo.java

--- src/jcrystal/configs/server/DebugInfo.java
+++ src/jcrystal/configs/server/DebugInfo.java
@@ -1,7 +1,11 @@
 package jcrystal.configs.server;
 
-public class DebugInfo {
+import java.io.Serializable;
 
+public class DebugInfo implements Serializable{
+
+	private static final long serialVersionUID = 1273793045508797206L;
+	
 	public boolean CORS = false;
 	public boolean ENTITY_CHECKS = false;
 	public boolean LOG_EXCEPTIONS = false;
