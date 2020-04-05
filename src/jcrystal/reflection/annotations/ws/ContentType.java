2f44cdde5ab4ea32be7c137610b1b54276902a90
gasotelo <gasotelo@gmail.com>
2020-04-05 16:42:54 -0500

    Update ContentType.java

--- src/jcrystal/reflection/annotations/ws/ContentType.java
+++ src/jcrystal/reflection/annotations/ws/ContentType.java
@@ -1,5 +1,22 @@
 package jcrystal.reflection.annotations.ws;
 
 public enum ContentType {
-	JSON, XML, UrlForm, MultipartForm, DEFAULT; 
+	JSON, XML, UrlForm, MultipartForm, DEFAULT;
+	public String getMimeType() {
+		switch (this) {
+			case JSON:
+				return "application/json";
+			case XML:
+				return "application/xml";
+			case MultipartForm:
+				return "multipart/form-data";
+			case UrlForm:
+				return "application/x-www-form-urlencoded";
+		default:
+			return null;
+		}
+	}
+	public boolean is(String contentType) {
+		return contentType.toLowerCase().contains(getMimeType());
+	}
 }
