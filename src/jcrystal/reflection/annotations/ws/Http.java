23f4b4bb00d5d5bbd86fc99a258f674ba498ed6d
gasotelo <gasotelo@gmail.com>
2020-04-05 21:25:13 -0500

    Update Http.java
    Retention(RUNTIME)

--- src/jcrystal/reflection/annotations/ws/Http.java
+++ src/jcrystal/reflection/annotations/ws/Http.java
@@ -1,5 +1,10 @@
 package jcrystal.reflection.annotations.ws;
 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+
+@Retention(RUNTIME)
 public @interface Http {
 	String path() default "";
 	Method method() default Method.DEFAULT;
