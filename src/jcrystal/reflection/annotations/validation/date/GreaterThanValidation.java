1f8ed976c61e00620d6e486d3b97f5a424e59530
gasotelo <gasotelo@gmail.com>
2020-05-02 15:54:20 -0500

    Date validation

---
+++ src/jcrystal/reflection/annotations/validation/date/GreaterThanValidation.java
@@ -0,0 +1,18 @@
+/* Copyright (C) Germán Augusto Sotelo Arévalo - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Germán Augusto Sotelo Arévalo <gasotelo@crystaltech.co>, December 2018
+ */
+package jcrystal.reflection.annotations.validation.date;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GreaterThanValidation {
+	boolean orEqual() default false;
+	boolean now() default false;
+	String field() default "";
+	String value() default "";
+	
+}
